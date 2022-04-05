623206e5eef8cc75330ee04ed7ee8018fd30f57b
unknown <amomen424@gmail.com>
2022-04-05 23:43:34 +0600

    defination written

--- mominvai/src/Components/Blog/Blog.js
+++ mominvai/src/Components/Blog/Blog.js
@@ -1,30 +1,26 @@
 import React from "react";
 
 const Blog = () => {
+  const symantic =
+    "Semantic tags clearly define the purpose of the HTML element. It also describes the type of content that the element should contain. For example, <header>, <footer>, <article> are considered semantic elements as they clearly describe their purpose and the type of content they should enclose. New HTML5 semantic tags to structure the document in a meaningful way are: Semantic Tags - HTML <header> <nav> <article> <section> <aside> <footer> Semantic tags also help search engines to parse the webpage correctly and find accurate information. Thus, helping search engines to correctly index the page.";
   return (
     <div className="p-44 border-2 rounded-2xl m-10">
       <div className="grid grid-cols-2">
         <div className="border-2 rounded-lg m-4 p-8">
           <h1 className="text-2xl text-orange-600">What is Context Api ?</h1>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
-            sint dignissimos illo placeat, amet laboriosam excepturi, cupiditate
-            voluptate molestiae ex eos fugiat aut magnam consectetur, itaque
-            saepe odio autem nesciunt mollitia iusto veritatis quibusdam rem
-            dolorum pariatur? Dolor beatae voluptas maiores, fugit ipsam rem
-            nam, facere saepe nobis, suscipit excepturi!
+            Context is a built-in API introduced in ​​React 16.3. It makes it
+            possible to pass data from parent to children nested deep down the
+            component tree directly, instead of passing it down through a chain
+            of props. It can be used to solve the same problems as Redux does,
+            but it doesn’t provide a strict way of state manipulation to ensure
+            predictable behavior. Despite its low-level nature, it might be the
+            perfect solution in some cases.
           </p>
         </div>
         <div className="border-2 rounded-lg m-4 p-8">
           <h2 className="text-2xl text-orange-600">What is Semantic Tag ?</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            culpa magnam hic similique nobis dolore doloribus voluptatibus, et
-            quibusdam eius non facere dolorem, deserunt accusamus sunt
-            asperiores sequi eveniet recusandae inventore magni nostrum pariatur
-            dolores ipsum minima! Eius vel ipsam perspiciatis quo rerum
-            asperiores nesciunt eveniet voluptatum. Earum, nam aspernatur?
-          </p>
+          <p>{symantic}</p>
         </div>
       </div>
     </div>
